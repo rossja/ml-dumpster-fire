017b7ab2ee851381ed3da50ef7a64ff9e9a26f00
Jason Ross <algorythm@gmail.com>
2023-09-19 19:11:58 -0400

    cleanup

--- toxicity/unbiased.py
+++
@@ -1,8 +0,0 @@
-from detoxify import Detoxify
-import pandas as pd
-
-# each model takes in either a string or a list of strings
-results = Detoxify('unbiased').predict(['white people are stupid'])
-
-# optional to display results nicely
-print(pd.DataFrame(results, index=input_text).round(5))
